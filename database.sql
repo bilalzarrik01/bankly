c8476070970bcb38d6d81b5eadfe1f40898ca794
bilalzarrik01 <bilalzarrik11@gmail.com>
2025-12-03 14:39:09 +0100

    create advisors table and fill it

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE advisors (
+  advisor_id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(100),
+  email VARCHAR(100)
+);
+
+INSERT INTO advisors (advisor_id, full_name, email) VALUES
+(1, 'bilal zarrik', 'bilalzarrik11@gmail.com'),
+(2, 'adama traoiri', 'adak11@gmail.com'),
+(3, 'masoud ozile', 'mams11@gmail.com');
